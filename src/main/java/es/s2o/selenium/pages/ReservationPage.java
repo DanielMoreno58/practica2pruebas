13d50d5e503b2fd5d663140d63f8fe6c67601d53
Daniel Moreno <daniel10697@hotmail.com>
2022-02-26 19:37:03 +0100

    No exponer webElements de la pagina principal

--- src/main/java/es/s2o/selenium/pages/ReservationPage.java
+++ src/main/java/es/s2o/selenium/pages/ReservationPage.java
@@ -4,6 +4,7 @@ import es.s2o.selenium.domain.FlightSearchDTO;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.Step;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,58 +23,64 @@ public class ReservationPage extends PageObjectBase {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @FindBy(id = "ensCloseBanner")
-    public WebElementFacade btnAcceptCookies;
+    private WebElementFacade btnAcceptCookies;
 
     @FindBy(className = "origin")
-    public WebElementFacade btnOrigin;
+    private WebElementFacade btnOrigin;
 
-    public WebElementFacade originInput;
+    private WebElementFacade originInput;
 
     @FindBy(css = "#popup-list > vy-airports-li > li:nth-child(1)")
-    public WebElementFacade originResult;
+    private WebElementFacade originResult;
 
-    public WebElementFacade destinationInput;
+    private WebElementFacade destinationInput;
 
     @FindBy(css = "#popup-list > vy-airports-li > li.liStation")
-    public WebElementFacade destinationResult;
+    private WebElementFacade destinationResult;
 
     @FindBy(css = "#searchbar > div > vy-datepicker-popup > vy-datepicker-header > ul > li:nth-child(2) > label")
-    public WebElementFacade oneWayTripSelect;
+    private WebElementFacade oneWayTripSelect;
 
     @FindBy(className = "ui-datepicker-next")
-    public WebElementFacade moveToNextCalendarMonth;
+    private WebElementFacade moveToNextCalendarMonth;
 
     @FindBy(xpath = "/html/body/div[4]/div[2]/flights-filter/div/div[2]/div/vy-datepicker-popup/vy-specificdates-datepicker/div/div[4]/table/tbody/tr[1]/td[3]/a")
-    public WebElementFacade selectDate;
+    private WebElementFacade selectDate;
 
     @FindBy(id = "btnSubmitHomeSearcher")
-    public WebElementFacade btnSearch;
+    private WebElementFacade btnSearch;
 
+    @Step
     public void acceptCookies(){
         btnAcceptCookies.click();
     }
 
+    @Step
     public void addInputOrigin(FlightSearchDTO flightSearchDTO){
         btnOrigin.click();
         typeInto(originInput, flightSearchDTO.getOrigen());
         originResult.click();
     }
 
+    @Step
     public void addInputDestination(FlightSearchDTO flightSearchDTO){
         typeInto(destinationInput, flightSearchDTO.getDestino());
         destinationResult.click();
     }
 
+    @Step
     public void addOneWayTripSelect(){
         oneWayTripSelect.waitUntilEnabled();
         oneWayTripSelect.click();
     }
 
+    @Step
     public void moveToNextCalendarMonth(){
         moveToNextCalendarMonth.waitUntilEnabled();
         moveToNextCalendarMonth.click();
     }
 
+    @Step
     public void selectDate(FlightSearchDTO flightSearchDTO) throws ParseException {
 
         Date today = new Date();
@@ -94,6 +101,7 @@ public class ReservationPage extends PageObjectBase {
         selectDate.click();
     }
 
+    @Step
     public void searchFlight(){
         btnSearch.click();
     }
